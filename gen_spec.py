7cbcaa0a144f456f4d887cdedcbdc639c41215b0
Brad Duncan <bradkyleduncan@gmail.com>
2019-10-23 10:57:36 +0200

    first commit

--- gen_spec.py
+++
@@ -1,8 +0,0 @@
-from webapp import create_app
-import json
-
-_, spec = create_app("conf/config.yml")
-
-if __name__ == "__main__":
-    with open("./spec.json", 'w') as f:
-        json.dump(spec.to_dict(), f)
--- webapp/default_config.py
+++
@@ -1,7 +0,0 @@
-default_config = {
-    "rethinkdb": {
-        "host": "db",
-        "port": 28015,
-        "db": "test"
-    }
-}
